37abb790938dfe644b4f219e220b0acf3ab23063
soomin0328 <tnalsdhksrms@naver.com>
2018-12-20 14:51:16 +0900

    AJAX

--- src/App.js
+++ src/App.js
@@ -7,49 +7,37 @@ class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        title:"Matrix",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L.jpg"
-      },
-      {
-        title:"Full Metal Jacket",
-        poster:"https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-      },
-      {
-        title:"Oldboy",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg"
-      },
-      {
-        title:"Star Wars",
-        poster:"https://imgc.allpostersimages.com/img/print/u-g-F8G58H0.jpg?w=300&h=450"
-      }
-    ]
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          ...this.state.movies,
-          {
-            title: "Trainspotting",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcHoWC2d7UF6xrRP5dlloaB_Hmbn5uvfNkJVBCAzgGK-h7b4Rn"
-          }
-        ]
-      })
-    }, 1000)
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
+function Movie({title, poster}){
+    return(
+      <div>
+        <MoviePoster poster={poster}/>
+        <h1>{title}</h1>
+    </div>  
+    )
+}
 
-static propTypes = {
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster"/>
+    )
+}
+
+Movie.propTypes = {
     title: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired
 }
 
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} alt={this.props.title}/>
-        )
-    }
-}
 export default Movie;
